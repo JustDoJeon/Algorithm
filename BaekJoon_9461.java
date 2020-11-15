15e7fddeebe8417ac4789545c3292f5f64a9b008
JeonDoHyun <wjsehgus1014@naver.com>
2020-11-15 23:47:22 +0900

    BaekJoon_Algo
    백준 알고리즘 커밋!!

--- BaekJoon_9461.java
+++ BaekJoon_9461.java
@@ -1,29 +1,2 @@
-package DP;
 
-import java.util.*;
-
-public class BaekJoon_9461 {
-
-	public static void main(String[] args) {
-
-		Scanner sc = new Scanner(System.in);
-		int t = sc.nextInt();
-		long[] d = new long[101];
-
-		d[0] = 0;
-		d[1] = 1;
 		d[2] = 1;
-		d[3] = 1;
-		d[4] = 2;
-		d[5] = 2;
-
-		for (int i = 6; i <= 100; i++) {
-			d[i] = d[i - 1] + d[i - 5];
-		}
-
-		while (t-- > 0) {
-			int n = sc.nextInt();
-			System.out.println(d[n]);
-		}
-	}
-}
