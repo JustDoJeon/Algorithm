739fb72e0ca2de86e16eb7aedae3e305a0115051
JeonDoHyun <wjsehgus1014@naver.com>
2020-11-16 22:30:53 +0900

    Create BaekJoon_2606.py

---
+++ BaekJoon/2606/BaekJoon_2606.py
@@ -0,0 +1,34 @@
+# 바이러스
+
+computer = int(input())
+network = int(input())
+
+virus_map = [ [ 0 for _ in range(computer+1 )]  for _ in range(computer+1)]
+#                   열                                         행
+
+
+print(virus_map)
+
+
+#2차원 배열 안에 넣어주기
+for _ in range(network):
+    x, y = map(int, input().split())
+    virus_map[x][y] = 1
+    virus_map[y][x] = 1
+
+
+def bfs(virus_map,start):
+    queue = [start]
+    visited =[]
+
+    while queue:
+        temp = queue.pop(0)
+        visited.append(temp)
+
+        for i in range(len(virus_map)):
+            if virus_map[temp][i] and i not in visited and i not in queue:
+                queue.append(i)
+
+    return len(visited) - 1
+
+print((bfs(virus_map,1)))
