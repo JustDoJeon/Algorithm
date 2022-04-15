72bda383863c8fe6fa9ce9de3e68139e43a88000
JeonDoHyun <52389219+JeonDoHyun@users.noreply.github.com>
2022-04-15 23:45:17 +0900

    Update S2_좌표압축.java
    시간초과 해결

--- BaekJoon/단계별문제/정렬/S2_좌표압축.java
+++ BaekJoon/단계별문제/정렬/S2_좌표압축.java
@@ -1,41 +1,48 @@
 package 단계별.정렬;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.Scanner;
+import java.util.StringTokenizer;
 
 public class S2_좌표압축 {
 
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-		int n = sc.nextInt();
+		int n = Integer.parseInt(br.readLine());
 
 		int[] arr = new int[n];
-
-		for (int i = 0; i < arr.length; i++) {
-			arr[i] = sc.nextInt();
+		int[] sorted = new int[n];
+		StringTokenizer st;
+		st = new StringTokenizer(br.readLine(), " ");
+		for (int i = 0; i < n; i++) {
+			sorted[i] = arr[i] = Integer.parseInt(st.nextToken());
 		}
-		ArrayList<Integer> list = new ArrayList<Integer>();
-		int mem = Integer.MIN_VALUE;
-		for (int i = 0; i < arr.length; i++) {
-			int a = arr[i];
-			int cnt = 0;
-			for (int j = 0; j < arr.length; j++) {
-				if (i != j) {
-					if (a > arr[j] && arr[j] != mem) {
-						mem = arr[j];
-						cnt++;
-					}
-				}
+
+		HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
+
+		Arrays.sort(sorted);
+
+		int rank = 0;
+		for (int v : sorted) {
+			if (!map.containsKey(v)) {
+				map.put(v, rank);
+				rank++;
 			}
-			list.add(cnt);
 		}
+		StringBuilder sb = new StringBuilder();
+		for (int key : arr) {
+			int ranking = map.get(key);
+			sb.append(ranking).append('\n');
+		}
+		
+		System.out.println(sb);
 
-		int[] res = new int[n];
-		res = list.stream().mapToInt(i -> i).toArray();
-		for (int i : res) {
-			System.out.print(i + " ");
-		}
 	}
 
 }
